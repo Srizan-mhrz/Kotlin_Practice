b5150e5cd4079aa9aebe6dc047ef9b4dd9d49f76
Srizan-mhrz <shrijan.mhrz@gmail.com>
2025-11-03 13:43:25 +0545

    ui practice

--- app/src/main/java/com/example/ai37a/MainActivity.kt
+++ app/src/main/java/com/example/ai37a/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.ColorRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.content.MediaType.Companion.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ai37a.ui.theme.AI37ATheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +38,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AI37ATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainBody()
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainBody() {
+    Column(
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize().background(color = colorResource(R.color.black))
+    ) {
+        Text(text = "Hello",style= TextStyle(
+            fontSize = 40.sp,
+            color = Color.White,
+            fontStyle = FontStyle.Italic,
+            textDecoration = TextDecoration.Underline,
+            fontWeight = FontWeight.Bold
+
+        ))
+        Text(text = "world",style= TextStyle(
+            fontSize = 40.sp,
+            color = Color.White,
+            fontStyle = FontStyle.Italic,
+            textDecoration = TextDecoration.Underline,
+            fontWeight = FontWeight.Bold
+        ))
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = null,
+            modifier = Modifier.size(160.dp)
+        )
+    }
+
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    AI37ATheme {
-        Greeting("Android")
-    }
+fun PreviewBody(){
+    MainBody()
 }
